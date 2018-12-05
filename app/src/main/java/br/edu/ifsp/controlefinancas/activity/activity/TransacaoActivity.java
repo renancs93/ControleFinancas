7d91231bfc5af6e5027e0b7c763142a7bab45b9c
Renan Silva <renancs93@gmail.com>
2018-12-05 17:12:07 -0200

    Menu Transacao, alteracaoes na view de Transações

--- app/src/main/java/br/edu/ifsp/controlefinancas/activity/activity/TransacaoActivity.java
+++ app/src/main/java/br/edu/ifsp/controlefinancas/activity/activity/TransacaoActivity.java
@@ -3,31 +3,42 @@ package br.edu.ifsp.controlefinancas.activity.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import br.edu.ifsp.controlefinancas.R;
 
 public class TransacaoActivity extends AppCompatActivity {
 
+    private String acao = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transacao);
 
         Intent intent = getIntent();
 
         if (intent != null){
 
-            String tipoLayout = intent.getAction();
+            acao = intent.getAction();
 
-            if (tipoLayout.equals(MainActivity.TAG_RECEITA)) {
-                setContentView(R.layout.activity_receita);
+            if (acao.equals(MainActivity.TAG_RECEITA)) {
                 getSupportActionBar().setTitle(getString(R.string.txtNova)+" "+getString(R.string.txtReceita));
             }
-            if (tipoLayout.equals(MainActivity.TAG_DESPESA)) {
-                //setContentView(R.layout.activity_receita);
+            if (acao.equals(MainActivity.TAG_DESPESA)) {
                 getSupportActionBar().setTitle(getString(R.string.txtNova)+" "+getString(R.string.txtDespesa));
             }
 
         }
+
+        //String[] SPINNERLIST = {"Android Material Design", "Material Design Spinner", "Spinner Using Material Library", "Material Spinner Example"};
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.categoria_transacao ,android.R.layout.simple_spinner_item);
+        MaterialBetterSpinner spinnerCategoria = (MaterialBetterSpinner) findViewById(R.id.spinner_categoria_transacao);
+        spinnerCategoria.setAdapter(arrayAdapter);
+
 
     }
 }
